24c7ea8ee76483b1d50aaf7b7f6be0c3331effd4
hnimmagadda <hnimmagadda@IN-5CG0251NNP>
2020-09-19 19:54:16 +0530

    spring jpa

---
+++ Project_01-Rest/src/main/java/com/dxc/Project01RestApplication.java
@@ -0,0 +1,13 @@
+package com.dxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project01RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project01RestApplication.class, args);
+	}
+
+}
